db350de441341c695898e4779219d12055458bed
alissone <alissond@acad.ifma.edu.br>
2020-06-23 22:23:19 -0300

    make clean

--- walk_forward/calculate_time.c
+++
@@ -1,7 +0,0 @@
-#include "calculate_time.h"
-#include <math.h>
-double distance(double distance) {
-   double distance_result;
-   distance_result = distance*(distance*(distance*(2.1259662352625052e-8 - 1.5674534848452648e-10*distance) + 8.8378740654481346e-6) + 0.17705100419700931) - 0.039715926468885276;
-   return distance_result;
-}
--- walk_forward/calculate_time.h
+++
@@ -1,4 +0,0 @@
-#ifndef PROJECT__CALCULATE_TIME__H
-#define PROJECT__CALCULATE_TIME__H
-double distance(double distance);
-#endif
