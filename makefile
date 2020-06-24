3ee37ad16300c9a2db8c8aa4db52ccfdf5ed31c0
alissone <alissond@acad.ifma.edu.br>
2020-06-23 21:52:42 -0300

    Separate into multiple files

---
+++ makefile
@@ -0,0 +1,6 @@
+distance:
+	python3 minecraft_walk_time_distance.py
+	gcc -o walk_time_distance calculate_time.c minecraft_walk_time_distance.c
+
+clean:
+	rm -rf walk_time_distance
--- minecraft_walk_time_distance.py
+++ minecraft_walk_time_distance.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 from sympy.utilities.codegen import codegen
 from sympy import symbols
@@ -19,15 +19,11 @@ z = np.polyfit(y_ax, x_ax, 4)
 f = np.poly1d(z)
 x_new = f(y_ax)
 
-# Plot prediction
-plt.style.use("ggplot")
-plt.scatter(x=x_ax, y=y_ax)
-plt.scatter(x_new, y_ax)
-plt.show()
+# # Plot prediction
+# plt.style.use("ggplot")
+# plt.scatter(x=x_ax, y=y_ax)
+# plt.scatter(x_new, y_ax)
+# plt.show()
 
-# Generate C code
-distance = symbols('distance')
-[(c_name, c_code), (h_name, c_header)] = \
-codegen(('distance', f(distance)), "C99", "test",
-         header=False, empty=False)
-print(c_code)
+from cfngen import save_as_c_func
+save_as_c_func(function=f, filename="calculate_time", var_name="distance")
